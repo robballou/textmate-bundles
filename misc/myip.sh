f46c95df053015dba1a3ce636c6924be50b4dcdc
robballou <rob.ballou@gmail.com>
2011-08-16 10:21:15 -0500

    Added shebang snippet (for adding the shebang statement for files) and myip script (to retrieve your current IP).

---
+++ misc/myip.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#
+# Insert your IP
+#
+# Requires curl installed
+#
+# Input: None
+# Output: Replace Selected Text
+# Activation: Tab Trigger, "myip"
+# Source selector: [blank]
+#
+
+curl -s http://whatismyip.org/
