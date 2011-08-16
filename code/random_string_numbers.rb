19b277c144eea9ad9f0bf05aec837bfa43c21406
robballou <rob.ballou@gmail.com>
2011-08-16 10:13:46 -0500

    Added random_string_numbers.rb to generate random string with numbers.

---
+++ code/random_string_numbers.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+#
+# Textmate command for inserting a random string with numbers
+#
+# Input: none
+# Output: Insert as text
+
+# based on: http://stackoverflow.com/questions/88311/how-best-to-generate-a-random-string-in-ruby
+o = [('a'..'z'),('A'..'Z'),(1..9)].map{|i| i.to_a}.flatten;  
+string = (0..50).map{ o[rand(o.length)]  }.join;
+print string.strip
